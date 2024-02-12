1b1a4a0d7a15cd56885673e824a6bac6039d11ea
Sphume <bulelo888@gmail.com>
2024-02-12 15:37:58 +0200

    End of Section 7

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,99 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import { v4 as uuid } from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [submitting, setSubmiting] = useState(false);
+  const {activityStore} = useStore();
 
   useEffect(() => {
-    agent.Activities.list().then(response => {
-      let activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0]; // split the text based on the T
-        activities.push(activity);
-      })
-      console.log(response);
-      setActivities(activities);
-      setLoading(false); // turn off the loading indicator
-    })
-  }, [])
+    activityStore.loadActivities();
+  }, [activityStore])
 
-  function handleSelectActivity(id: string) {
-    setSelectedActivity(activities.find(x => x.id === id)); // x holds the activity that make up activities array and it should eaul the id
-  }
-
-  function handleCancelSelectActivity() {
-    setSelectedActivity(undefined);
-  }
-
-  function handleFormOpen(id?: string) {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity(); // if 'id' is populated, left side will be implemented. if not, then right side will
-    setEditMode(true);
-  }
-
-  function handleFormClose() {
-    setEditMode(false);
-  }
-
-  function handleCreateOrEditActivity(activity: Activity) {
-    setSubmiting(true); // set to true to start the loading indicators
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity]); // sets activities
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmiting(false);
-      })
-    } else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, { ...activity, id: uuid() }]);
-        setSelectedActivity(activity);
-        setSubmiting(false);
-        setEditMode(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmiting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmiting(false);
-    })
-  }
-
-  if (loading) return <LoadingComponent content='Loading App'/>
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading App'/>
 
   return (
     <>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEdit={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
+        
+        <ActivityDashboard/>
       </Container>
 
     </>
   )
 }
 
-export default App
+export default observer(App);
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,10 +1,9 @@
 import { Button, Container, Menu } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
-interface Props {
-    openForm: () => void; // takes no parameters because it creates activity
-}
+export default function NavBar() {
+    const {activityStore} = useStore();
 
-export default function NavBar({openForm}: Props) {
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -16,7 +15,7 @@ export default function NavBar({openForm}: Props) {
                 <Menu.Item name='Activities'/>
 
                 <Menu.Item>
-                    <Button onClick={openForm} positive content='Create Activity'/>
+                    <Button onClick={() => activityStore.openForm()} positive content='Create Activity'/>
                 </Menu.Item>
             </Container>
         </Menu>
