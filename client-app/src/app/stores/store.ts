8d589ccbb491f88a47cc49da6808c4a4f3850780
Sphume <bulelo888@gmail.com>
2024-02-21 16:45:36 +0200

    End of Seciton 17

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -1,21 +1,24 @@
 import { createContext, useContext } from "react";
 import ActivityStore from "./activityStore";
-import CommonStore from "./CommonStore";
 import UserStore from "./userStore";
 import ModalStore from "./modalStore";
+import CommonStore from "./commonStore";
+import ProfileStore from "./profileStore";
 
 interface Store {
     activityStore: ActivityStore;
     commonStore: CommonStore;
     userStore: UserStore;
     modalStore: ModalStore;
+    profileStore: ProfileStore;
 }
 
 export const store: Store = {
     activityStore: new ActivityStore(),
     commonStore: new CommonStore(),
     userStore: new UserStore(),
-    modalStore: new ModalStore()
+    modalStore: new ModalStore(),
+    profileStore: new ProfileStore()
 }
 
 export const StoreContext = createContext(store);
